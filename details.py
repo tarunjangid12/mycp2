e8769fef2e1dc7e1bd921b4082a8350649b5a224
tarunjangid12 <146462019+tarunjangid12@users.noreply.github.com>
2023-12-14 16:08:09 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,10 +1,5 @@
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_ID = int(getenv("22779671", 0))
-API_HASH = getenv("125d8d88b77309dc3b154cbbfc2dacb2", None)
-BOT_TOKEN = getenv("6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c", None)
-OWNER_ID = int(getenv("OWNER_ID", 1713924419))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1713924419").split()))
+API_ID = 22779671
+API_HASH = "125d8d88b77309dc3b154cbbfc2dacb2"
+BOT_TOKEN = "6378365862:AAEU4TLL_MUh7Imzc-wmRAD2rfvz11BjdJE"
+OWNER_ID = 5518407661
+SUDO_USERS = 5518407661
